0efd237dbbc77b9b93ce3c9e505c75f97a813ee8
Abhi-Yadu1C <152044989+Abhi-Yadu1C@users.noreply.github.com>
2023-12-07 09:16:39 +0000

    dataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+console.log("Hitesh")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "hitesh")
